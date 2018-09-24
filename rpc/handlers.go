c7f2f9fb75c715c7b4336ada94827db31b4864e8
Tam Le <ldmtam@gmail.com>
2018-09-24 23:22:49 +0700

    add sign raw tx endpoint

--- rpc/handlers.go
+++ rpc/handlers.go
@@ -13,26 +13,10 @@ import (
 
 	"golang.org/x/crypto/ed25519"
 
+	"github.com/simpleblockchain/account"
 	"github.com/simpleblockchain/core/transaction"
 	log "github.com/sirupsen/logrus"
 )
-
-type keypair struct {
-	PrivateKey string `json:"private_key"`
-	PublicKey  string `json:"public_key"`
-}
-
-type createRawTx struct {
-	From  string `json:"from"`
-	To    string `json:"to"`
-	Value string `json:"value"`
-	Nonce string `json:"nonce"`
-}
-
-type signRawTx struct {
-	PrivateKey string `json:"private_key"`
-	RawTx      string `json:"raw_tx"`
-}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -40,6 +24,11 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func generateKeypairHandler(w http.ResponseWriter, r *http.Request) {
+	type keypair struct {
+		PrivateKey string `json:"private_key"`
+		PublicKey  string `json:"public_key"`
+	}
+
 	pubKeyBytes, privKeyBytes, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		log.WithFields(log.Fields{
@@ -58,20 +47,28 @@ func generateKeypairHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func sendRawTxHandler(w http.ResponseWriter, r *http.Request) {
+	type sendRawTx struct {
+		Signature string `json:"signature"`
+	}
 
-}
-
-func createRawTxHandler(w http.ResponseWriter, r *http.Request) {
-	data := new(createRawTx)
+	data := new(sendRawTx)
 	b, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(b, &data)
 
-	// log.WithFields(log.Fields{
-	// 	"from":  data.From,
-	// 	"to":    data.To,
-	// 	"value": data.Value,
-	// 	"nonce": data.Nonce,
-	// }).Info("Values received from createRawTxHandler")
+	fmt.Println("signature: ", data.Signature)
+}
+
+func createRawTxHandler(w http.ResponseWriter, r *http.Request) {
+	type createRawTx struct {
+		From  string `json:"from"`
+		To    string `json:"to"`
+		Value string `json:"value"`
+		Nonce string `json:"nonce"`
+	}
+
+	data := new(createRawTx)
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &data)
 
 	txFrom, err := hex.DecodeString(data.From)
 	if err != nil {
@@ -106,7 +103,9 @@ func createRawTxHandler(w http.ResponseWriter, r *http.Request) {
 		time.Now().Unix(),
 	)
 	if err != nil {
-		log.Error("cannot create new raw transaction")
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("cannot create new raw transaction")
 	}
 
 	txHashInHash := tx.Hash()
@@ -127,10 +126,66 @@ func createRawTxHandler(w http.ResponseWriter, r *http.Request) {
 		}).Error("cannot encode transaction")
 	}
 
-	d := map[string]string{"tx_hash": hex.EncodeToString([]byte(pbMess))}
+	d := map[string]string{"tx_hash": hex.EncodeToString(pbMess)}
 	json.NewEncoder(w).Encode(d)
 }
 
 func signRawTxHandler(w http.ResponseWriter, r *http.Request) {
+	type signRawTx struct {
+		PrivateKey string `json:"private_key"`
+		RawTx      string `json:"raw_tx"`
+	}
 
+	data := new(signRawTx)
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &data)
+
+	log.WithFields(log.Fields{
+		"Private key": data.PrivateKey,
+		"Raw Tx":      data.RawTx,
+	}).Info("Value received from sign raw tx")
+
+	tx := new(transaction.TxImpl)
+	rawTxBytes, err := hex.DecodeString(data.RawTx)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("cannot convert tx hex string to bytes")
+	}
+
+	err = tx.Unmarshal(rawTxBytes)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("cannot decode transaction")
+	}
+
+	privKeyBytes, err := hex.DecodeString(data.PrivateKey)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("cannot decode private key hex string to bytes")
+	}
+
+	kp := &account.KeyPairImpl{
+		PrivateKey: ed25519.PrivateKey(privKeyBytes),
+	}
+
+	tx.Sign(kp)
+
+	log.WithFields(log.Fields{
+		"Signature": hex.EncodeToString(tx.Signature()),
+	}).Info("Signature of the message")
+
+	// pubKeyBytes, err := hex.DecodeString(data.PublicKey)
+	// if err != nil {
+	// 	log.WithFields(log.Fields{
+	// 		"error": err,
+	// 	}).Error("cannot decode public key hex string to bytes")
+	// }
+
+	// isVerified := tx.Verify(pubKeyBytes)
+	// fmt.Println(isVerified)
+	d := map[string]string{"signature": hex.EncodeToString(tx.Signature())}
+	json.NewEncoder(w).Encode(d)
 }
